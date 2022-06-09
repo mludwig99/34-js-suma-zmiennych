0dba0070323cbd2ef03283b9485b63cc7d88629c
mludwig99 <michalludwig99@gmail.com>
2022-06-09 17:25:19 +0200

    34-js-suma

---
+++ script/main.js
@@ -0,0 +1,15 @@
+let firstNumber = 12;
+let secNumber = 16;
+
+function calc() {
+    let result = 7 + 5;
+    return result;
+}
+//Nie byłem pewien pierwszego rozwiazania wiec przygotowałem drugie
+let firstNumber2 = 12;
+let secNumber2 = 16;
+
+function calc2(a, b) {
+    let result = a + b;
+    return result;
+}
